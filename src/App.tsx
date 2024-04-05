1ba3de49b83e95ff73a1d6c7ef70c371ea2b3dd5
rishabhknowss <rishabhrai1515@gmail.com>
2024-04-05 21:07:10 +0530

    randomised

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   const [rightOption, setRightOption] = useState(0);
 
   const handleOptionsChange = () => {
-    alert("50% people choose this option")
+    alert(`${Math.floor(Math.random() * 100) + 1}% people choose this option`)
     setLeftOption(prevOption => (prevOption + 1) % leftOptions.length);
     setRightOption(prevOption => (prevOption + 1) % rightOptions.length);
   };
@@ -31,20 +31,20 @@ function App() {
   ];
 
   return (
-    <>
+    <div className='custom-cursor'>
       <div className="flex justify-center text-white bg-black py-4">
         <img src={logo} alt="Logo" className="w-12 h-12 rounded-md" />
         <h1 className="text-xl font-bold flex flex-col justify-center mx-2">Would you rather</h1>
       </div>
       <div className="grid grid-cols-2 h-screen">
-        <div className="flex flex-col justify-center text-center bg-blue-400 text-2xl font-bold" onClick={handleOptionsChange}>
+        <div className="flex flex-col justify-center text-center bg-blue-400 hover:bg-blue-500 text-2xl font-bold" onClick={handleOptionsChange}>
           <Left option={leftOptions[leftOption]} />
         </div>
-        <div className="flex flex-col justify-center text-center bg-red-400 text-2xl font-extrabold" onClick={handleOptionsChange}>
+        <div className="flex flex-col justify-center text-center bg-red-400 hover:bg-red-500 text-2xl font-extrabold" onClick={handleOptionsChange}>
           <Right option={rightOptions[rightOption]} />
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
